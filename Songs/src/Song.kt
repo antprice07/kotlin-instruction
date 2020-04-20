36cd33604956160feb38d113d4f0be774d80e116
Anthony Price <antprice07@gmail.com>
2020-04-20 14:46:59 -0400

    different ways to initialize values in a constructor

--- Songs/src/Song.kt
+++ Songs/src/Song.kt
@@ -18,3 +18,30 @@ fun main() {
     songTwo.stop()
     songThree.play()
 }
+
+
+//Showing how a constructor can be used in different ways to assign values
+//class Dog(val name: String, var weight: Int, breed_param: String) {
+//    old declaration
+class Dog (val name: String, weight_param: Int, breed_param: String) {
+    var activities = arrayOf("Walks")
+    val breed = breed_param.toUpperCase()
+    var weight = weight_param
+        set(value){
+            //field refers to the property's backing field (underlying value of the prop) and prevents endless loops
+            if (value > 0) field = value
+        }
+    val weightInKgs: Double
+        get() = weight/2.2
+
+    //Properties must be initialized to run the code, but can be handled later if used with the prefix lateinit as long as it's a var
+
+    //Example of an initializer block
+    init {
+        println("Dog $name has been created.")
+    }
+    //Can have more than one initializer block that runs in order of creation
+    init {
+        println("The breed is $breed")
+    }
+}
